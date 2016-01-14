65aaa4e3de7a5db4f60f950ff17e2c5eb76af7c8
PavelN <pavel.najman@gmail.com>
2016-01-14 23:02:22 +0100

    TG-21 #comment added event model, mysql user has to have enough privileges to use foreign keys

--- model.py
+++ model.py
@@ -1,36 +1,35 @@
 import os
 import sys
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-from sqlalchemy.types import BLOB,REAL
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+from datetime import datetime 
  
-Base = declarative_base()
- 
-class Person(Base):
-    __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    surename = Column(String(250),nullable=False)
-    password = Column(String(250),nullable=False)
-    picture = Column(BLOB)
-    email = Column(String(250),nullable=False)
-    mobile = Column(String(15))
-    address = Column(String(250))
-    rating = Column(REAL)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    surename = db.Column(db.String(250),nullable=False)
+    username = db.Column(db.String(250), nullable=False,unique=True)
+    password = db.Column(db.String(250),nullable=False)
+    picture = db.Column(db.LargeBinary)
+    email = db.Column(db.String(250),nullable=False,unique=True)
+    mobile = db.Column(db.String(15))
+    address = db.Column(db.String(250))
+    rating = db.Column(db.Float)
+    joindate = db.Column(db.DateTime)
+    
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    photo = db.Column(db.LargeBinary)
+    active = db.Column(db.Boolean)
+    price = db.Column(db.Float,nullable=False)
+    address = db.Column(db.String(250),nullable=False)
+    date_time_create = db.Column(db.DateTime)
+    date_time_close = db.Column(db.DateTime)
+    accessories_purchased = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    repairman_id = db.Column(db.Integer)
+    
+db.create_all()
 
-#!!! if you do not have database, execute following two lines
-engine = create_engine('mysql+pymysql://workuser:work1234@localhost') # login to the Mysql
-engine.execute("CREATE DATABASE work") #create db
-engine.execute("USE work") # select new db
-
-#!!! if you have your database (in this case called work) user following line
-#engine = create_engine('mysql+pymysql://workuser:work1234@localhost\work')
-
-
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
-Base.metadata.create_all(engine)
