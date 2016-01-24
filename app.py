54f6a64fd0068b641c02ebabafd05663ce038336
Igor Kljucaricek <igor.kljucaricek@gmail.com>
2016-01-24 16:16:26 +0100

    Modified files for routing and start server

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import logging
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 
 app.secret_key = '26s3uqr&v2@dt@93%*79biuao@)zlmmi)^^p*jycr#!&ydg_ok7l78'
@@ -11,7 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://workuser:work1234@local
 
 db = SQLAlchemy(app)
 
-#from views import *
+from views import *
+
 
 if __name__ == "__main__":
     logging.basicConfig(filename='logs.log', level=logging.DEBUG)
--- views.py
+++ views.py
@@ -1,7 +1,11 @@
 from flask import request, render_template, Response, g, session, redirect, url_for
 from app import app
-from models import User, Event # from database
+from model import User, Event # from database
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/signin')
+def login():
+	return render_template('signin.html')
