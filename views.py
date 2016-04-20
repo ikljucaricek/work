7661514cadeb28475ee1b27cfdb8fc65456a4167
PavelN <pavel.najman@gmail.com>
2016-04-20 20:45:42 +0200

    TG-84 search and paging are on the same line and sending of email is corrected, sent to both client and repairman once repairman is chosen

--- views.py
+++ views.py
@@ -175,22 +175,9 @@ def chooserm():
         
         fromaddr = 'tygayoinc@gmail.com'
         toaddrs  = rm.email
-        msg = "\r\n".join([
-                "From: tygayoinc@gmail.com",
-                "To: "+ rm.email,
-                "Subject: Chosen to "+event.name,
-                "",
-                "Dear "+ rm.name+",",
-                "",
-                "This mail is from Tygayo Inc. You have been chosen to take care of "+event.name+".",
-                "The event takes place at "+event.address+", scheduled for "+str(event.date_time_create)+".",
-                "",
-                "The event details are in the link bellow:",
-                "http://localhost:5000"+url_for('showevent', id=eventid),
-                "",
-                "Best Regards,",
-                "TygAyo Inc."
-                ])
+        toaddrs_client = client.email
+        msg = confirmation_mail('repairman', rm, client, event, eventid)
+        msg_client = confirmation_mail('client', rm, client, event, eventid)
         username = 'tygayoinc@gmail.com'
         password = 'Work1234'
         server = SMTP("smtp.gmail.com",587)
@@ -322,14 +309,14 @@ def allevents():
             return render_template('events.html', username = session['username'], events = Event.get_all()[::-1])
     return render_template('events.html', username = session['username'], events = Event.get_all()[::-1])
 
-def confirmation_mail(msg_for_what, user_obj, event_obj, eventid):
+def confirmation_mail(msg_for_what, rm , client, event_obj, eventid):
     if msg_for_what == 'repairman':
         chosen_repariman_msg = "\r\n".join([
             "From: tygayoinc@gmail.com",
-            "To: " + user_obj.email,
+            "To: " + rm.email,
             "Subject: Chosen to " + event_obj.name,
             "",
-            "Dear " + user_obj.name + ",",
+            "Dear " + rm.name + ",",
             "",
             "This mail is from Tygayo Inc. You have been chosen to take care of " + event_obj.name + ".",
             "The event takes place at " + event_obj.address + ", scheduled for " + str(event_obj.date_time_create) + ".",
@@ -345,12 +332,12 @@ def confirmation_mail(msg_for_what, user_obj, event_obj, eventid):
     if msg_for_what == 'client':
         msg_to_client = "\r\n".join([
                 "From: tygayoinc@gmail.com",
-                "To: " + user_obj.email,
+                "To: " +  client.email,
                 "Subject: Chose repairman for " + event_obj.name,
                 "",
-                "Dear " + user_obj.name + ",",
+                "Dear " + client.name + ",",
                 "",
-                "This mail is from Tygayo Inc. You choose " + user_obj.name + " for repairman.",
+                "This mail is from Tygayo Inc. You choose " + rm.name + " for repairman.",
                 "The event takes place at " + event_obj.address + ", scheduled for " + str(event_obj.date_time_create) + ".",
                 "",
                 "The event details are in the link bellow:",
