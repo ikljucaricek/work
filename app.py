2b98192fcdf17dd620d1863ac808fa8d50e82e8f
PavelN <pavel.najman@gmail.com>
2017-04-13 14:03:39 +0200

    adding port 3308 to the connection to local mariaDB so that it can be run from USB

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 import logging, os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -10,7 +10,7 @@ babel = Babel(app)
 
 app.secret_key = '26s3uqr&v2@dt@93%*79biuao@)zlmmi)^^p*jycr#!&ydg_ok7l78'
 if os.environ.get('JAWSDB_MARIA_URL') is None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://workuser:work1234@localhost/work'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://workuser:work1234@localhost:3308/work'
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['JAWSDB_MARIA_URL']
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fglzebctdqphod:aK6qOhgTPt-e2JSQP05DKpes2S@ec2-54-247-185-241.eu-west-1.compute.amazonaws.com:5432/debfq3v7p66b39'
